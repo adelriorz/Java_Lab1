ea3f4864e87e34988aa3ca9d1af1f488a3ae913f
Armando <adelriorz@gmail.com>
2022-10-05 23:47:13 -0500

    Adding final task

--- JavaLab1/src/main/java/LabExcersice1/PrintPattern.java
+++ JavaLab1/src/main/java/LabExcersice1/PrintPattern.java
@@ -5,4 +5,14 @@ package LabExcersice1;
  */
 public class PrintPattern {
     
+    public static void displayJavaHarcoded(){
+        System.out.println("   J    a   v     v  a\n" +
+        "   J   a a   v   v  a a\n" +
+        "J  J  aaaaa   V V  aaaaa\n" +
+        " JJ  a     a   V  a     a");
+    }
+    
+    public static void main(String[] args) {
+        displayJavaHarcoded();
+    }
 }
